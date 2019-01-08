890d831e17d431ff0c5defa0bbfaaae70be52b60
Mimo <mimosaha@gmail.com>
2019-01-08 12:34:46 +0600

    update manifest and Add application file

---
+++ app/src/main/java/com/m_network/NetworkApplication.java
@@ -0,0 +1,33 @@
+package com.m_network;
+
+import android.support.multidex.MultiDexApplication;
+
+
+/**
+ * * ============================================================================
+ * * Copyright (C) 2019 W3 Engineers Ltd - All Rights Reserved.
+ * * Unauthorized copying of this file, via any medium is strictly prohibited
+ * * Proprietary and confidential
+ * * ----------------------------------------------------------------------------
+ * * Created by: Mimo Saha on [08-Jan-2019 at 12:32 PM].
+ * * Email: mimosaha@w3engineers.com
+ * * ----------------------------------------------------------------------------
+ * * Project: M_Network.
+ * * Code Responsibility: <Purpose of code>
+ * * ----------------------------------------------------------------------------
+ * * Edited by :
+ * * --> <First Editor> on [08-Jan-2019 at 12:32 PM].
+ * * --> <Second Editor> on [08-Jan-2019 at 12:32 PM].
+ * * ----------------------------------------------------------------------------
+ * * Reviewed by :
+ * * --> <First Reviewer> on [08-Jan-2019 at 12:32 PM].
+ * * --> <Second Reviewer> on [08-Jan-2019 at 12:32 PM].
+ * * ============================================================================
+ **/
+public class NetworkApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
